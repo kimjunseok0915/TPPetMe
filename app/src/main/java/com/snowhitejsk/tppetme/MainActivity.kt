dfb5ddacad71f62a0f5b761cbe3648aa195b0f0e
kimjunseok0915 <tommy20020915@gmail.com>
2022-11-04 16:04:13 +0900

    frag.facil frag.missing frag.community

--- app/src/main/java/com/snowhitejsk/tppetme/MainActivity.kt
+++ app/src/main/java/com/snowhitejsk/tppetme/MainActivity.kt
@@ -2,10 +2,20 @@ package com.snowhitejsk.tppetme
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
+import com.snowhitejsk.tppetme.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        NavigationUI.setupWithNavController(binding.navBar,findNavController(R.id.nav_host))
+
     }
 }
